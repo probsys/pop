a4539ee79cd8c7853106d111c26c0617318f26ae
Feras A. Saad <fsaad@cmu.edu>
2023-09-07 17:35:31 -0400

    Push to master on deploy.

--- deploy.sh
+++ deploy.sh
@@ -6,5 +6,7 @@ SITEDIR=$(realpath $(dirname -- "$0"))/_site/
 SERVER=linux.gp.cs.cmu.edu
 TARGET='/afs/cs.cmu.edu/project/pop/www/'
 
+git push origin master
+
 bundle exec jekyll build
 rsync -ah --info=progress2 "${SITEDIR}" ${SERVER}:"${TARGET}"
